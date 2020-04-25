98c6045087004caa59507865394d67ccc240b2d0
Abinash Sinha <abinashsinha330@gmail.com>
2020-04-25 05:09:25 -0500

    Update product_region_sales_partition.sql

--- product_region_sales_partition.sql
+++ product_region_sales_partition.sql
@@ -11,14 +11,14 @@ productname varchar,
 price decimal(38,4),
 quantity int,
 amount decimal(38,4),
-orderdate TIMESTAMP
+day int
 )
 PARTITIONED BY (region varchar, year int, month int)
 COMMENT 'Impala Parquet sales and region materialized table partitioned by region, year and month'
 STORED AS Parquet;
 
 INSERT INTO cr7_sales.product_region_sales_partition PARTITION(region, year, month)
-SELECT ps.orderid, ps.salespersonid, ps.customerid, ps.productid, ps.productname, ps.price, ps.quantity, ps.amount, ps.orderdate, e.region, ps.year, ps.month
+SELECT ps.orderid, ps.salespersonid, ps.customerid, ps.productid, ps.productname, ps.price, ps.quantity, ps.amount, ps.day, e.region, ps.year, ps.month
 FROM cr7_sales.product_sales_partition ps
 JOIN cr7_sales.employees e
 ON ps.salespersonid = e.employeeid;
