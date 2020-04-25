d9717109e7812689e9cba735f4e399af6d553e7c
Abinash Sinha <abinashsinha330@gmail.com>
2020-04-25 05:08:52 -0500

    Update product_sales_partition.sql

--- product_sales_partition.sql
+++ product_sales_partition.sql
@@ -11,14 +11,14 @@ productname varchar,
 price decimal(38,4), 
 quantity int, 
 amount decimal(38,4),
-orderdate TIMESTAMP 
+day int 
 ) 
 PARTITIONED BY (year int, month int)
 COMMENT 'Impala Parquet product and sales materialized table partitioned by year and month'
 STORED AS Parquet;
 
 INSERT INTO cr7_sales.product_sales_partition PARTITION(year, month)
-SELECT s.orderid, s.salespersonid, s.customerid, p.productid, p.name, p.price, s.quantity, s.quantity * p.price, s.datesale, year(s.datesale) AS year, month(s.datesale) AS month
+SELECT s.orderid, s.salespersonid, s.customerid, p.productid, p.name, p.price, s.quantity, s.quantity * p.price, day(s.datesale) AS day, year(s.datesale) AS year, month(s.datesale) AS month
 FROM cr7_sales.products p
 JOIN cr7_sales.sales s
 ON p.productid = s.productid;
